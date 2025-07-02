12e7cb88c5985d94e00e82a1ff37de868a850f7f
Anshu-nits <anshurajjaiswal2003@gmail.com>
2025-07-02 13:16:31 +0530

    final commit

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -13,7 +13,7 @@ export const protectRoute = async (req, res, next) => {
 
     if (!decoded) {
       return res.status(401).json({ message: "Unauthorized - Invalid token" });
-    }
+    } 
 
     const user = await User.findById(decoded.userId).select("-password");
 
